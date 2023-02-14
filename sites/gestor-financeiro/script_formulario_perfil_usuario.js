ea2e6a63a55ead7d4503f8eea393b41c73d6f4a1
Rafael Menezes <faelgmp@gmail.com>
2023-02-14 20:06:20 -0300

    distribuição do salario com base no perfil

--- sites/gestor-financeiro/script_formulario_perfil_usuario.js
+++ sites/gestor-financeiro/script_formulario_perfil_usuario.js
@@ -53,28 +53,217 @@ function gerarResultado(){
     if(document.querySelector("input[name='rendaExtra']:checked").value == "sim"){
         const tempo = document.querySelector("input[name='tempo']:checked").value;
 
+        fraseResultado = "<h1>O que melhorar?</h1>"
+
         if(tempo == "preciso"){
-            fraseResultado += "";    
+            fraseResultado += "<p>Observamos que dedicar um tempo para fazer aquilo que você gosta pode aumentar a sua produtividade.</p>";    
         }else if(tempo == "naoTenho"){
-            fraseResultado += "";
+            fraseResultado += "<p>Observamos que reorganizar a sua agenda te traria uma melhor qualidade de vida.</p>";
         }else if(tempo == "fimSemana"){
-            fraseResultado += "";
+            fraseResultado += "<p>Você possui uma boa gestão do seu tempo.</p>";
         }else{
-            fraseResultado += "";
+            fraseResultado += "<p>Observamos que investir em cursos, aprender algo novo, pode ser algo bom para você.</p>";
         };
 
     }else{
+        const tempo = document.querySelector("input[name='tempo']:checked").value;
+
+        fraseResultado = "<h1>O que melhorar?</h1>"
 
         if(tempo == "preciso"){
-            fraseResultado += "";    
+            fraseResultado += "<p>Notamos que uma boa estratégia de organização pode melhorar vários aspectos da sua vida.</p>";    
         }else if(tempo == "naoTenho"){
-            fraseResultado += "";
+            fraseResultado += "<p>Seria bom otimizar melhor o seu tempo para que possa fazer um curso, aprender algo novo, ampliando a sua área de conhecimento.</p>";
         }else if(tempo == "fimSemana"){
-            fraseResultado += "";
+            fraseResultado += "<p>Procurar uma renda extra pode ser uma boa opção para seu tempo livre.</p>";
         }else{
-            fraseResultado += "";
+            fraseResultado += "<p>Obter uma renda extra e aprender algo novo pode ser duas metas para você.</p>";
         };
 
     };
 
-};
+
+
+
+    if(document.querySelector("input[name='compras']:checked").value == "cartao"){
+        usuario.caixa += 10;
+        usuario.investimento += 5;
+        usuario.empreender += 0;
+        usuario.lazer += 0;
+        usuario.compras += 0;
+
+    }else{
+        usuario.caixa += 5;
+        usuario.investimento += 10;
+        usuario.empreender += 0;
+        usuario.lazer += 0;
+        usuario.compras += 0;
+
+    };
+
+    if(document.querySelector("input[name='divisaoRenda']:checked").value == "100"){
+        usuario.caixa += 7;
+        usuario.investimento += 1;
+        usuario.empreender += 1;
+        usuario.lazer += 3;
+        usuario.compras += 3;
+
+    }else if(document.querySelector("input[name='divisaoRenda']:checked").value == "50"){
+        usuario.caixa += 4;
+        usuario.investimento += 4;
+        usuario.empreender += 3;
+        usuario.lazer += 2;
+        usuario.compras += 2;
+
+    }else if(document.querySelector("input[name='divisaoRenda']:checked").value == "25"){
+        usuario.caixa += 2;
+        usuario.investimento += 4;
+        usuario.empreender += 5;
+        usuario.lazer += 2;
+        usuario.compras += 2;
+
+    }else{
+        usuario.caixa += 2;
+        usuario.investimento += 4;
+        usuario.empreender += 7;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    };
+
+    if(document.querySelector("input[name='lazer']:checked").value == "casa"){
+        usuario.caixa += 6;
+        usuario.investimento += 3;
+        usuario.empreender += 3;
+        usuario.lazer += 2;
+        usuario.compras += 1;
+
+    }else if(document.querySelector("input[name='lazer']:checked").value == "shopping"){
+        usuario.caixa += 4;
+        usuario.investimento += 3;
+        usuario.empreender += 3;
+        usuario.lazer += 2;
+        usuario.compras += 3;
+
+    }else{
+        usuario.caixa += 3;
+        usuario.investimento += 3;
+        usuario.empreender += 2;
+        usuario.lazer += 4;
+        usuario.compras += 3;
+
+    };
+
+    if(document.querySelector("input[name='compraAluguel']:checked").value == "avista"){
+        usuario.caixa += 7;
+        usuario.investimento += 3;
+        usuario.empreender += 3;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    }else if(document.querySelector("input[name='compraAluguel']:checked").value == "financiada"){
+        usuario.caixa += 4;
+        usuario.investimento += 4;
+        usuario.empreender += 4;
+        usuario.lazer += 2;
+        usuario.compras += 1;
+
+    }else{
+        usuario.caixa += 2;
+        usuario.investimento += 5;
+        usuario.empreender += 5;
+        usuario.lazer += 1;
+        usuario.compras += 2;
+
+    };
+
+    if(document.querySelector("input[name='prazo']:checked").value == "curto"){
+        usuario.caixa += 6;
+        usuario.investimento += 1;
+        usuario.empreender += 1;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    }else if(document.querySelector("input[name='prazo']:checked").value == "medio"){
+        usuario.caixa += 3;
+        usuario.investimento += 2;
+        usuario.empreender += 3;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    }else{
+        usuario.caixa += 2;
+        usuario.investimento += 4;
+        usuario.empreender += 2;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    };
+
+    if(document.querySelector("input[name='investidor']:checked").value == "naoApto"){
+        usuario.caixa += 9;
+        usuario.investimento += 0;
+        usuario.empreender += 0;
+        usuario.lazer += 3;
+        usuario.compras += 3;
+
+    }else if(document.querySelector("input[name='investidor']:checked").value == "conservador"){
+        usuario.caixa += 7;
+        usuario.investimento += 2;
+        usuario.empreender += 2;
+        usuario.lazer += 2;
+        usuario.compras += 2;
+
+    }else if(document.querySelector("input[name='investidor']:checked").value == "moderado"){
+        usuario.caixa += 5;
+        usuario.investimento += 4;
+        usuario.empreender += 4;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    }else if(document.querySelector("input[name='investidor']:checked").value == "agressivo"){
+        usuario.caixa += 3;
+        usuario.investimento += 7;
+        usuario.empreender += 4;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    }else{
+        usuario.caixa += 2;
+        usuario.investimento += 9;
+        usuario.empreender += 2;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    };
+
+    if(document.querySelector("input[name='trabalhar']:checked").value == "carteira"){
+        usuario.caixa += 13;
+        usuario.investimento += 0;
+        usuario.empreender += 0;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    }else if(document.querySelector("input[name='trabalhar']:checked").value == "empreender"){
+        usuario.caixa += 2;
+        usuario.investimento += 0;
+        usuario.empreender += 11;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    }else{
+        usuario.caixa += 2;
+        usuario.investimento += 11;
+        usuario.empreender += 0;
+        usuario.lazer += 1;
+        usuario.compras += 1;
+
+    };
+
+   
+
+
+    const output = document.querySelector("main > form > output");
+
+    output.innerHTML = `${fraseResultado}`;
+}
